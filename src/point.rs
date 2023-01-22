7db1bce484cc811bf511a4e8d4dfa8ca7dc629b2
Juniper Parsons <juniperparsnips@gmail.com>
2023-01-22 15:29:10 -0500

    Impl PartialEq for types

--- src/point.rs
+++ src/point.rs
@@ -1,10 +1,12 @@
 use std::convert::From;
-use std::ops::{ Add, Sub };
+use std::ops::{Add, Mul, Sub};
 
-use graphics::math::{ Scalar, Vec2d };
+use graphics::math::{Scalar, Vec2d};
+
+use crate::Size;
 
 /// A point in the Cartesian plane.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Point {
     /// The x coordinate.
     pub x: Scalar,
@@ -16,7 +18,10 @@ impl Add<Scalar> for Point {
     type Output = Point;
 
     fn add(self, s: Scalar) -> Point {
-        Point { x: self.x + s, y: self.y + s }
+        Point {
+            x: self.x + s,
+            y: self.y + s,
+        }
     }
 }
 
@@ -25,7 +30,10 @@ impl<T: Into<Point>> Add<T> for Point {
 
     fn add(self, v: T) -> Point {
         let v: Point = v.into();
-        Point { x: self.x + v.x, y: self.y + v.y }
+        Point {
+            x: self.x + v.x,
+            y: self.y + v.y,
+        }
     }
 }
 
@@ -47,11 +55,23 @@ impl From<(Scalar, Scalar)> for Point {
     }
 }
 
+impl From<Size> for Point {
+    fn from(size: Size) -> Point {
+        Point {
+            x: size.w,
+            y: size.h,
+        }
+    }
+}
+
 impl Sub<Scalar> for Point {
     type Output = Point;
 
     fn sub(self, s: Scalar) -> Point {
-        Point { x: self.x - s, y: self.y - s }
+        Point {
+            x: self.x - s,
+            y: self.y - s,
+        }
     }
 }
 
@@ -59,7 +79,21 @@ impl<T: Into<Point>> Sub<T> for Point {
     type Output = Point;
 
     fn sub(self, v: T) -> Point {
-        let v = v.into();
-        Point { x: self.x - v.x, y: self.y - v.y }
+        let v: Point = v.into();
+        Point {
+            x: self.x - v.x,
+            y: self.y - v.y,
+        }
+    }
+}
+
+impl Mul<Scalar> for Point {
+    type Output = Point;
+
+    fn mul(self, s: Scalar) -> Self::Output {
+        Point {
+            x: self.x * s,
+            y: self.y * s,
+        }
     }
 }
